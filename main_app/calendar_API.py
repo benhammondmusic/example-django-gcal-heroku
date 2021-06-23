919e531ed64bb434f1dd25bcf88e7aa7c24ae665
BenHammondMusic <41567007+benhammondmusic@users.noreply.github.com>
2021-06-23 15:30:38 -0600

    updates, ready for deploy

--- main_app/calendar_API.py
+++ main_app/calendar_API.py
@@ -3,32 +3,20 @@ from google.oauth2 import service_account
 import googleapiclient.discovery
 import datetime
 
-# CAL_ID = config('CAL_ID')
+CAL_ID = config('CAL_ID')
 SCOPES = ['https://www.googleapis.com/auth/calendar']
 SERVICE_ACCOUNT_FILE = './google-credentials.json'
 
 
 def test_calendar():
     print("RUNNING TEST_CALENDAR()")
-    return ["this", "is", "a", "test"]
     
-"""     credentials = service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+    credentials = service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=SCOPES)
     service = googleapiclient.discovery.build('calendar', 'v3', credentials=credentials)
-
-    # GET ALL EXISTING EVENTS
-    events_result = service.events().list(calendarId=CAL_ID, maxResults=2500).execute()
-    events = events_result.get('items', [])
-    
-    # LOG THEM ALL OUT IN DEV TOOLS CONSOLE
-    for e in events:
-        event_id = e['id']
-        print(event_id)
-        # service.events().delete(calendarId=CAL_ID, eventId=event_id).execute()
-        
 
     # CREATE A NEW EVENT
     new_event = {
-    'summary': "Ben Hammond's Super Awesome Event",
+    'summary': "Ben Hammond Tech's Super Awesome Event",
     'location': 'Denver, CO USA',
     'description': 'https://benhammond.tech',
     'start': {
@@ -42,4 +30,21 @@ def test_calendar():
     }
     service.events().insert(calendarId=CAL_ID, body=new_event).execute()
     print('Event created')
- """
+
+    # GET ALL EXISTING EVENTS
+    events_result = service.events().list(calendarId=CAL_ID, maxResults=2500).execute()
+    events = events_result.get('items', [])
+    
+    # LOG THEM ALL OUT IN DEV TOOLS CONSOLE
+    for e in events:
+        print(e)
+        # event_id = e['id']
+        # service.events().delete(calendarId=CAL_ID, eventId=event_id).execute()
+    
+    return events
+
+"""     test_event1 = {"start": {"date": "2022-01-01"}, "end": {"date": "2022-01-07"}, "summary":"test event 1"}
+    test_event2 = {"start": {"date": "2022-02-01"}, "end": {"date": "2022-02-07"}, "summary":"test event 2"}
+    events = [test_event1, test_event2] """
+
+    
--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from .calendar_API import test_calendar
 
-
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
